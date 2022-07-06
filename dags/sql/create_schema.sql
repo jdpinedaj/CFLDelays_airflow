51e0eb8688c531c35cf0085819fdbdf3e714594a
Juan Pineda-Jaramillo <juandpineda@gmail.com>
2022-07-06 15:22:08 +0200

    additional dags

--- dags/sql/create_schema.sql
+++ dags/sql/create_schema.sql
@@ -1,2 +1,1 @@
---DROP SCHEMA IF EXISTS {{ params.schema_name }} CASCADE;
 CREATE SCHEMA IF NOT EXISTS {{ params.schema_name }};
--- dags/workflow_processing_training.py
+++ dags/workflow_processing_training.py
@@ -67,7 +67,7 @@ POSTGRES_DBNAME = 'cfl'
 ##! 3. Instantiate a DAG
 #######################
 
-dag = DAG(dag_id='CFL_delay_prediction_v2',
+dag = DAG(dag_id='CFL_delay_prediction_v2_processing_training',
           description='CFL_delay_prediction',
           start_date=datetime(2022, 6, 9),
           schedule_interval=SCHEDULE_INTERVAL,
@@ -79,319 +79,17 @@ dag = DAG(dag_id='CFL_delay_prediction_v2',
 ##! 4. Tasks
 #######################
 
-#? 4.1. Starting pipeline
+#? 4.1. Checking tables
 
-start_pipeline = DummyOperator(
-    task_id='start_pipeline',
+check_postgres_tables = PostgresOperator(
+    task_id='check_postgres_tables',
+    postgres_conn_id='postgres_default',
+    sql='sql/creation_tables/check_postgres_tables.sql',
+    params={'table_name': 'cfl.public.stations_countries'},
     dag=dag,
 )
 
-#? 4.2. Creating empty tables
-
-with TaskGroup(
-        'create_tables',
-        dag=dag,
-) as create_tables:
-
-    create_public_schema = PostgresOperator(
-        task_id="create_public_schema",
-        postgres_conn_id='postgres_default',
-        sql='sql/create_schema.sql',
-        params={'schema_name': 'public'},
-        dag=dag,
-    )
-
-    create_incident_concerne_table = PostgresOperator(
-        task_id="create_incident_concerne_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_incident_concerne_table.sql',
-        params={'table_name': 'cfl.public.incident_concerne'},
-        dag=dag,
-    )
-
-    create_incidents_table = PostgresOperator(
-        task_id="create_incidents_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_incidents_table.sql',
-        params={'table_name': 'cfl.public.incidents'},
-        dag=dag,
-    )
-
-    create_station_table = PostgresOperator(
-        task_id="create_station_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_station_table.sql',
-        params={'table_name': 'cfl.public.station'},
-        dag=dag,
-    )
-
-    create_terminal_table = PostgresOperator(
-        task_id="create_terminal_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_terminal_table.sql',
-        params={'table_name': 'cfl.public.terminal'},
-        dag=dag,
-    )
-
-    create_train_etape_table = PostgresOperator(
-        task_id="create_train_etape_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_train_etape_table.sql',
-        params={'table_name': 'cfl.public.train_etape'},
-        dag=dag,
-    )
-
-    create_train_jalon_table = PostgresOperator(
-        task_id="create_train_jalon_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_train_jalon_table.sql',
-        params={'table_name': 'cfl.public.train_jalon'},
-        dag=dag,
-    )
-
-    create_train_lot_table = PostgresOperator(
-        task_id="create_train_lot_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_train_lot_table.sql',
-        params={'table_name': 'cfl.public.train_lot'},
-        dag=dag,
-    )
-
-    create_train_position_table = PostgresOperator(
-        task_id="create_train_position_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_train_position_table.sql',
-        params={'table_name': 'cfl.public.train_position'},
-        dag=dag,
-    )
-
-    create_train_wagon_table = PostgresOperator(
-        task_id="create_train_wagon_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_train_wagon_table.sql',
-        params={'table_name': 'cfl.public.train_wagon'},
-        dag=dag,
-    )
-
-    create_train_table = PostgresOperator(
-        task_id="create_train_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_train_table.sql',
-        params={'table_name': 'cfl.public.train'},
-        dag=dag,
-    )
-
-    create_wagon_capacite_table = PostgresOperator(
-        task_id="create_wagon_capacite_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_wagon_capacite_table.sql',
-        params={'table_name': 'cfl.public.wagon_capacite'},
-        dag=dag,
-    )
-
-    create_wagon_position_table = PostgresOperator(
-        task_id="create_wagon_position_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_wagon_position_table.sql',
-        params={'table_name': 'cfl.public.wagon_position'},
-        dag=dag,
-    )
-
-    create_wagon_table = PostgresOperator(
-        task_id="create_wagon_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_wagon_table.sql',
-        params={'table_name': 'cfl.public.wagon'},
-        dag=dag,
-    )
-
-    create_wagon_modele_table = PostgresOperator(
-        task_id="create_wagon_modele_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_wagon_modele_table.sql',
-        params={'table_name': 'cfl.public.wagon_modele'},
-        dag=dag,
-    )
-
-    create_stations_countries_table = PostgresOperator(
-        task_id="create_stations_countries_table",
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/create_stations_countries_table.sql',
-        params={'table_name': 'cfl.public.stations_countries'},
-        dag=dag,
-    )
-
-    create_public_schema >> [
-        create_incident_concerne_table, create_incidents_table,
-        create_station_table, create_terminal_table, create_train_etape_table,
-        create_train_jalon_table, create_train_lot_table,
-        create_train_position_table, create_train_wagon_table,
-        create_train_table, create_wagon_capacite_table,
-        create_wagon_position_table, create_wagon_table,
-        create_wagon_modele_table, create_stations_countries_table
-    ]
-
-#? 4.3. Populating tables
-
-with TaskGroup(
-        'populate_tables',
-        dag=dag,
-) as populate_tables:
-
-    populate_incident_concerne_table = ExcelToPostgresOperator(
-        task_id='populate_incident_concerne_table',
-        target_table='cfl.public.incident_concerne',
-        file_name='INCIDENT_CONCERNE.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_incidents_table = ExcelToPostgresOperator(
-        task_id='populate_incidents_table',
-        target_table='cfl.public.incidents',
-        file_name='INCIDENTS.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_station_table = ExcelToPostgresOperator(
-        task_id='populate_station_table',
-        target_table='cfl.public.station',
-        file_name='STATION.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_terminal_table = ExcelToPostgresOperator(
-        task_id='populate_terminal_table',
-        target_table='cfl.public.terminal',
-        file_name='TERMINAL.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_train_etape_table = ExcelToPostgresOperator(
-        task_id='populate_train_etape_table',
-        target_table='cfl.public.train_etape',
-        file_name='TRAIN_ETAPE.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_train_jalon_table = ExcelToPostgresOperator(
-        task_id='populate_train_jalon_table',
-        target_table='cfl.public.train_jalon',
-        file_name='TRAIN_JALON.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_train_lot_table = ExcelToPostgresOperator(
-        task_id='populate_train_lot_table',
-        target_table='cfl.public.train_lot',
-        file_name='TRAIN_LOT.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_train_position_table = ExcelToPostgresOperator(
-        task_id='populate_train_position_table',
-        target_table='cfl.public.train_position',
-        file_name='TRAIN_POSITION.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_train_wagon_table = ExcelToPostgresOperator(
-        task_id='populate_train_wagon_table',
-        target_table='cfl.public.train_wagon',
-        file_name='TRAIN_WAGON.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_train_table = ExcelToPostgresOperator(
-        task_id='populate_train_table',
-        target_table='cfl.public.train',
-        file_name='TRAIN.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_wagon_capacite_table = ExcelToPostgresOperator(
-        task_id='populate_wagon_capacite_table',
-        target_table='cfl.public.wagon_capacite',
-        file_name='WAGON_CAPACITE.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_wagon_position_table = ExcelToPostgresOperator(
-        task_id='populate_wagon_position_table',
-        target_table='cfl.public.wagon_position',
-        file_name='WAGON_POSITION.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_wagon_table = ExcelToPostgresOperator(
-        task_id='populate_wagon_table',
-        target_table='cfl.public.wagon',
-        file_name='WAGON.xlsx',
-        sheet_name=0,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_wagon_modele_table = ExcelToPostgresOperator(
-        task_id='populate_wagon_modele_table',
-        target_table='cfl.public.wagon_modele',
-        file_name='WAGON.xlsx',
-        sheet_name=1,
-        identifier='id',
-        dag=dag,
-    )
-
-    populate_stations_countries_table = PostgresOperator(
-        task_id='populate_stations_countries_table',
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/populate_stations_countries_table.sql',
-        params={'table_name': 'cfl.public.stations_countries'},
-        dag=dag,
-    )
-
-    check_postgres_tables = PostgresOperator(
-        task_id='check_postgres_tables',
-        postgres_conn_id='postgres_default',
-        sql='sql/creation_tables/check_postgres_tables.sql',
-        params={'table_name': 'cfl.public.stations_countries'},
-        dag=dag,
-    )
-
-    [
-        populate_incident_concerne_table, populate_incidents_table,
-        populate_station_table, populate_terminal_table,
-        populate_train_etape_table, populate_train_jalon_table,
-        populate_train_lot_table, populate_train_position_table,
-        populate_train_wagon_table, populate_train_table,
-        populate_wagon_capacite_table, populate_wagon_position_table,
-        populate_wagon_table, populate_wagon_modele_table,
-        populate_stations_countries_table
-    ] >> check_postgres_tables
-
-#? 4.4. Preprocessing tables
+#? 4.2. Preprocessing tables
 
 with TaskGroup(
         'preprocessing_tables',
@@ -504,7 +202,7 @@ with TaskGroup(
         dag=dag,
     )
 
-    create_public_processed_schema >> [
+    check_postgres_tables >> [
         preprocess_incident_concerne_table, preprocess_incidents_table,
         preprocess_station_table, preprocess_train_etape_table,
         preprocess_train_jalon_table, preprocess_train_lot_table,
@@ -512,7 +210,7 @@ with TaskGroup(
         preprocess_stations_countries_table
     ]
 
-#? 4.5. Merging data - Creating incident_data, station_data, train_data and wagon_data tables and checking data
+#? 4.3. Merging data - Creating incident_data, station_data, train_data and wagon_data tables and checking data
 
 with TaskGroup(
         'merging_data',
@@ -563,7 +261,7 @@ with TaskGroup(
         check_train_data_final_table, check_wagon_data_table
     ]
 
-#? 4.6. Preparing dataset for ML
+#? 4.4. Preparing dataset for ML
 
 with TaskGroup(
         'data_for_ml',
@@ -595,7 +293,7 @@ with TaskGroup(
 
     create_public_ready_for_ml_schema >> preparing_for_ml >> check_data_ready_for_ml
 
-#? 4.7. ETL process
+#? 4.5. ETL process
 
 with TaskGroup(
         'etl_process',
@@ -627,7 +325,7 @@ with TaskGroup(
 
     create_public_etl_schema >> etl_pipeline >> check_data_etl
 
-#? 4.8. ML process
+#? 4.6. ML process
 
 with TaskGroup(
         'ml_process',
@@ -664,7 +362,7 @@ with TaskGroup(
 
     postgres_to_pandas >> training_ML_model
 
-#? 4.9. Predicting
+#? 4.7. Predicting
 
 predicting_with_model = PythonOperator(
     task_id="predicting_with_model",
@@ -684,28 +382,8 @@ predicting_with_model = PythonOperator(
 ##! 5. Setting up dependencies
 #######################
 
-# Starting pipeline
-start_pipeline >> create_tables
-
-# Populating postgres tables
-create_incident_concerne_table >> populate_incident_concerne_table
-create_incidents_table >> populate_incidents_table
-create_station_table >> populate_station_table
-create_terminal_table >> populate_terminal_table
-create_train_etape_table >> populate_train_etape_table
-create_train_jalon_table >> populate_train_jalon_table
-create_train_lot_table >> populate_train_lot_table
-create_train_position_table >> populate_train_position_table
-create_train_wagon_table >> populate_train_wagon_table
-create_train_table >> populate_train_table
-create_wagon_capacite_table >> populate_wagon_capacite_table
-create_wagon_position_table >> populate_wagon_position_table
-create_wagon_table >> populate_wagon_table
-create_wagon_modele_table >> populate_wagon_modele_table
-create_stations_countries_table >> populate_stations_countries_table
-
 # Preprocessing tables and storing them in public_processed schema
-populate_tables >> preprocessing_tables
+check_postgres_tables >> preprocessing_tables
 
 # Joining tables in order to create incident_data, station_data, train_data and wagon_data tables
 preprocessing_tables >> merging_data
